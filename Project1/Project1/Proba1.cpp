142d9df600982dc6770b681b1462297d2cdfd4f7
Student 241483 <241483@student.pwr.edu.pl>
2019-03-21 00:53:23 +0100

    time

--- Project1/Project1/Proba1.cpp
+++ Project1/Project1/Proba1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
-#include <time.h>
+//#include <time.h>
+#include <ctime>
 
 #include "insertionSort.hpp"
 #include "bubbleSort.hpp"
@@ -10,6 +11,11 @@
 
 int main()
 {
+	double roznica;
+	std::cout.setf(std::ios::fixed);
+	std::cout.precision(5);
+	clock_t start, koniec;
+
 	int rozmiar;
 
 	std::cout << "Podaj rozmiar tablicy: " << "\n";
@@ -22,13 +28,18 @@ int main()
 	for (int i = 0; i < rozmiar; ++i)
 	{
 		tablica[i] = rand();
-		std::cout << tablica[i] << " ";
+		//std::cout << tablica[i] << " ";
 	}
 
-
+	start = clock();
 	//bubbleSort(tablica, rozmiar);
 	//insertionSort(tablica, rozmiar);
-	quickSort(tablica, 0, rozmiar - 1);
+	//quickSort(tablica, 0, rozmiar - 1);
+	
+
+	koniec = clock();
+	roznica = (koniec - start) / (double)CLOCKS_PER_SEC;
+	std::cout << "Czas wykonania: " << roznica << "\n";
 
 	std::cout << "posortowana: " << "\n";
 	for (int i = 0; i < rozmiar; ++i)
@@ -38,6 +49,7 @@ int main()
 
 	delete[] tablica;
 	system("pause");
+	return 0;
 }
 
 
